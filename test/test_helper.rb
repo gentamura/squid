59fad7c29a3d95b1ba38e5095117b4980afde847
Gen Tamura <gen.tamura84@gmail.com>
2016-02-16 10:18:16 +0000

    Add a persistent login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,8 +8,25 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
   def is_logged_in?
     !session[:user_id].nil?
+  end
+
+  def log_in_as(user, options = {})
+    password = options[:password] || 'foobarbaz'
+    if integration_test?
+      post login_path, params: {
+        session: {
+          email: user.email,
+          password: password
+        }
+      }
+    else
+      session[:user_id] = user.id
+    end
+  end
+
+  def integration_test?
+    defined? open_session
   end
 end
