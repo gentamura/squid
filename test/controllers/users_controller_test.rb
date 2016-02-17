adf2fff3e264e51e302a1b80e18910bb1ccc6300
Gen Tamura <gen.tamura84@gmail.com>
2016-02-17 02:44:38 +0000

    Add don't allow update admin status via the web

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -12,13 +12,13 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should redirect edit when not logged in" do
-    get edit_user_path id: @user
+    get edit_user_path(@user)
     assert_not flash.empty?
     assert_redirected_to login_url
   end
 
   test "should redirect update when not logged in" do
-    patch user_path id: @user, params: {
+    patch user_path(@user), params: {
       user: {
         name: @user.name,
         email: @user.email
@@ -30,14 +30,14 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test "should redirect edit when logged in as wrong user" do
     log_in_as(@other_user)
-    get edit_user_path id: @user
+    get edit_user_path(@user)
     assert flash.empty?
     assert_redirected_to root_url
   end
 
   test "should redirect update when logged in as wrong user" do
     log_in_as(@other_user)
-    patch user_path id: @user, params: {
+    patch user_path(@user), params: {
       user: {
         name: @user.name,
         email: @user.email
@@ -54,7 +54,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test "should redirect destroy when not logged in" do
     assert_no_difference 'User.count' do
-      delete user_path id: @user
+      delete user_path(@user)
     end
     assert_redirected_to login_url
   end
@@ -62,8 +62,22 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should redirect destroy when logged in as a non-admin user" do
     log_in_as(@other_user)
     assert_no_difference 'User.count' do
-      delete user_path id: @user
+      delete user_path(@user)
     end
     assert_redirected_to root_url
+  end
+
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: {
+      user: {
+        password: "foobarbaz",
+        password_confirmation: "foobarbaz",
+        admin: true
+      }
+    }
+    @other_user.reload
+    assert_not @other_user.admin?
   end
 end
