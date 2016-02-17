392ded70b1652fde93aad5399d1100d09bcc0de3
Gen Tamura <gen.tamura84@gmail.com>
2016-02-17 10:50:58 +0000

    Add account activation

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
   def setup
-    @user = users(:foo)
+    @activated_user = users(:foo)
+    @non_activated_user = users(:baz)
   end
 
   test "login with invalid information" do
@@ -17,13 +18,13 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
   test "login with valid information followed by logout" do
     get login_path
-    post login_path, params: { session: { email: @user.email, password: "foobarbaz" } }
+    post login_path, params: { session: { email: @activated_user.email, password: "foobarbaz" } }
     assert is_logged_in?
-    assert_redirected_to @user
+    assert_redirected_to @activated_user
     follow_redirect!
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
-    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", user_path(@activated_user)
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
@@ -32,11 +33,46 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path, count: 0
-    assert_select "a[href=?]", user_path(@user), count: 0
+    assert_select "a[href=?]", user_path(@activated_user), count: 0
   end
 
   test "user login with remembering" do
-    log_in_as(@user)
+    log_in_as(@activated_user)
     assert_not_nil cookies['remember_token']
   end
+
+  test "user login with not activated" do
+    log_in_as(@non_activated_user)
+    assert_not is_logged_in?
+  end
+
+  test "user login with invalid activation token" do
+    user = @non_activated_user
+    assign_activation(user)
+    get edit_account_activation_path("invalid token")
+    assert_not is_logged_in?
+  end
+
+  test "user login with invalid email" do
+    user = @non_activated_user
+    assign_activation(user)
+    get edit_account_activation_path(user.activation_token, email: 'wrong')
+    assert_not is_logged_in?
+  end
+
+  test "user login with valid activation token" do
+    user = @non_activated_user
+    assign_activation(user)
+    get edit_account_activation_path(user.activation_token, email: user.email)
+    follow_redirect!
+    assert_select 'div.alert-success', 'Account activated!'
+    assert_select 'h1', test: 'User page'
+    assert is_logged_in?
+  end
+
+  private
+    def assign_activation(user)
+      user.send(:create_activation_digest)
+      user.update_attribute(:activation_digest, user.activation_digest)
+    end
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
-
+require 'pp'
 class UsersSignupTest < ActionDispatch::IntegrationTest
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
+
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
@@ -27,9 +31,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
           password_confirmation: "foobarbaz"
         }
       }
-      follow_redirect!
     end
-    assert_select 'h1', test: 'User page'
-    assert is_logged_in?
+    assert_equal 1, ActionMailer::Base.deliveries.size
   end
+
 end
